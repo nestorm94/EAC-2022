6239142a5005de7c44a77c0f2981d91125b6f999
nestorm94 <nestorm9408@gmail.com>
2022-02-28 22:43:06 -0500

    AJUSTES EN FORNT DE MATERIAL

--- src/app/components/dashboard/modulo1/modulo1.component.ts
+++ src/app/components/dashboard/modulo1/modulo1.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+
+import {FormControl, Validators} from '@angular/forms';
+
+interface department {
+  value: string;
+  viewValue: string;
+}
+interface municipio {
+  value: string;
+  viewValue: string;
+}
+interface tipOrg {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-modulo1',
   templateUrl: './modulo1.component.html',
   styleUrls: ['./modulo1.component.css']
 })
 export class Modulo1Component implements OnInit {
-  formModulo1: FormGroup;
-  constructor() { 
-    const today = new Date();
-    const month = today.getMonth();
-    const year = today.getFullYear();
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
-    this.formModulo1 = new FormGroup({
-      start: new FormControl(new Date(year, month, 13)),
-      end: new FormControl(new Date(year, month, 16)),
-    });
+  departments: municipio[] = [
+    {value: 'DP01', viewValue: 'AMAZONAS'},
+    {value: 'DP02', viewValue: 'CASANARE'},
+    {value: 'DP03', viewValue: 'META'},
+  ];
 
-    
-
-  }
+  municipios: municipio[] = [
+    {value: 'MN01', viewValue: 'TAURAMENA'},
+    {value: 'MN02', viewValue: 'ACUAZUL'},
+    {value: 'MN03', viewValue: 'YOPAL'},
+  ];
+  tipoOrgns: tipOrg[] = [
+    {value: 'MN01', viewValue: 'SAS'},
+    {value: 'MN02', viewValue: 'LIMITDA'},
+    {value: 'MN03', viewValue: 'CONSORCIO'},
+  ];
+  constructor() {}
 
   ngOnInit(): void {
   }
